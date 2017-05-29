140bafad7a6be9647be8181a1795c02f237ca1c1
François Schmidts <francois.schmidts@gmail.com>
2017-05-29 12:17:26 +0200

    new implementation

--- json_feed.py
+++ json_feed.py
@@ -8,96 +8,137 @@ A Pelican plugin to generate a JSON Feed file
 
 from __future__ import unicode_literals
 
-import os.path
 import json
-from bs4 import BeautifulSoup
-from datetime import datetime
-from pytz import timezone
-from codecs import open
-try:
-    from urlparse import urljoin
-except ImportError:
-    from urllib.parse import urljoin
+from jinja2 import Markup
+from operator import attrgetter
+from pelican import signals, generators, writers
 
-from pelican import signals
 
-def format_date(date):
-    if date.tzinfo:
-        tz = date.strftime('%z')
-        tz = tz[:-2] + ':' + tz[-2:]
-    else:
-        tz = "-00:00"
-    return date.strftime("%Y-%m-%dT%H:%M:%S") + tz
+class JSONFeedGenerator(generators.ArticlesGenerator):
 
-class JSONFeedGenerator(object):
+    def generate_feeds(self, writer):
+        """Generate the feeds from the current context, and output files."""
 
-    def __init__(self, context, settings, path, theme, output_path, *null):
+        if self.settings.get('FEED_JSON'):
+            writer.write_feed(self.articles, self.context,
+                              self.settings['FEED_JSON'], feed_type='json')
 
-        self.output_path = output_path
-        self.context = context
-        self.now = datetime.now()
-        self.siteurl = settings.get('SITEURL')
-        self.author = settings.get('AUTHOR')
-        self.feed_title = settings.get('SITENAME')
-        self.feed_description = settings.get('JSON_FEED_DESCRIPTION')
-        self.feed_icon = settings.get('JSON_FEED_ICON')
-        self.json_nodes = []
+        if self.settings.get('FEED_ALL_JSON'):
+            all_articles = list(self.articles)
+            for article in self.articles:
+                all_articles.extend(article.translations)
+            all_articles.sort(key=attrgetter('date'), reverse=True)
 
-        self.default_timezone = settings.get('TIMEZONE', 'UTC')
-        self.timezone = getattr(self, 'timezone', self.default_timezone)
-        self.timezone = timezone(self.timezone)
+            writer.write_feed(all_articles, self.context,
+                                self.settings['FEED_ALL_JSON'],
+                                feed_type='json')
 
-    def create_json_node(self, page):
+        for cat, arts in self.categories:
+            arts.sort(key=attrgetter('date'), reverse=True)
+            if self.settings.get('CATEGORY_FEED_JSON'):
+                writer.write_feed(arts, self.context,
+                                  self.settings['CATEGORY_FEED_JSON']
+                                  % cat.slug, feed_title=cat.name,
+                                  feed_type='json')
 
-        if getattr(page, 'status', 'published') != 'published':
-            return
+        for auth, arts in self.authors:
+            arts.sort(key=attrgetter('date'), reverse=True)
+            if self.settings.get('AUTHOR_FEED_JSON'):
+                writer.write_feed(arts, self.context,
+                                  self.settings['AUTHOR_FEED_JSON']
+                                  % auth.slug, feed_title=auth.name,
+                                  feed_type='json')
 
-        soup_title = BeautifulSoup(page.title.replace('&nbsp;', ' '), 'html.parser')
-        page_title = soup_title.get_text(' ', strip=True)
+        if self.settings.get('TAG_FEED_JSON'):
+            for tag, arts in self.tags.items():
+                arts.sort(key=attrgetter('date'), reverse=True)
+                writer.write_feed(arts, self.context,
+                                    self.settings['TAG_FEED_JSON'] % tag.slug,
+                                    feed_title=tag.name, feed_type='json')
 
-        soup_text = BeautifulSoup(page.content, 'html.parser')
-        page_text = str(soup_text)
+        if self.settings.get('TRANSLATION_FEED_JSON'):
+            translations_feeds = defaultdict(list)
+            for article in chain(self.articles, self.translations):
+                translations_feeds[article.lang].append(article)
 
-        page_url = self.siteurl + '/' + page.url
-
-        page_date = getattr(page, 'date', self.now)
-
-        node = {'id': page_url,
-                'url': page_url,
-                'title': page_title,
-                'content_html': page_text,
-                'date_published': format_date(page_date),
-                'author': {'name': self.author}
-                }
-
-        self.json_nodes.append(node)
+            for lang, items in translations_feeds.items():
+                items.sort(key=attrgetter('date'), reverse=True)
+                if self.settings.get('TRANSLATION_FEED_JSON'):
+                    writer.write_feed(
+                        items, self.context,
+                        self.settings['TRANSLATION_FEED_JSON'] % lang,
+                        feed_type='json')
 
     def generate_output(self, writer):
-        path = os.path.join(self.output_path, 'feed.json')
+        self.generate_feeds(writer)
 
-        pages = self.context['articles']
 
-        for article in self.context['articles']:
-            pages += article.translations
+class JSONFeedWriter(writers.Writer, object):
 
-        for page in pages:
-            self.create_json_node(page)
+    def _create_new_feed(self, feed_type, feed_title, context):
+        if feed_type != 'json':
+            return super(JSONFeedWriter, self)\
+                    ._create_new_feed(feed_type, feed_title, context)
+        if feed_title:
+            feed_title = context['SITENAME'] + ' - ' + feed_title
+        else:
+            feed_title = context['SITENAME']
 
-        feed = {'version': 'https://jsonfeed.org/version/1',
-                'title': self.feed_title,
-                'home_page_url': self.siteurl,
-                'feed_url': self.siteurl + '/feed.json',
-                'description': self.feed_description,
-                'icon': self.feed_icon,
-                'items': self.json_nodes}
+        feed = JSONFeed(title=Markup(feed_title).striptags(),
+                        link=(self.site_url + '/'),
+                        feed_url=self.feed_url,
+                        author=context.get('AUTHOR'),
+                        favicon=context.get('FAVICON'),
+                        icon=context.get('SITELOGO'),
+                        description=context.get('SITESUBTITLE', ''))
+        return feed
 
-        with open(path, 'w', encoding='utf-8') as fd:
-            json.dump(feed, fd, indent=4, ensure_ascii=False)
+
+
+class JSONFeed(object):
+
+    def __init__(self, title, link, feed_url,
+            author, favicon, icon, description):
+        self.feed = {"version": "https://jsonfeed.org/version/1",
+                     "title": title,
+                     "home_page_url": link,
+                     "feed_url": feed_url,
+                     "description": description,
+                     "items": []}
+        if author:
+            self.feed['author'] = {'name': author}
+        if favicon:
+            self.feed['favicon'] = favicon
+        if icon:
+            self.feed['icon'] = icon
+
+    def add_item(self, title, link, unique_id, description,
+            content, categories, author_name, pubdate, updateddate):
+
+        item = {'id': unique_id,
+                'url': link,
+                'title': title,
+                'content_html': content,
+                'description': description,
+                'date_published': pubdate.isoformat(),
+                'tags': [str(cat) for cat in categories],
+                'author': {'name': str(author_name)}}
+        if updateddate:
+            item['date_modified'] = updateddate.isoformat()
+        self.feed['items'].append(item)
+
+    def write(self, fp, encoding='utf-8'):
+        json.dump(self.feed, fp, encoding)
 
 
 def get_generators(generators):
     return JSONFeedGenerator
 
 
+def get_writer(writer):
+    return JSONFeedWriter
+
+
 def register():
+    signals.get_writer.connect(get_writer)
     signals.get_generators.connect(get_generators)
